c3f4224b87f233c52a1e5a0d3a3b1f9983fec2df
dspashintsev <dspashintsev@avito.ru>
2024-09-11 21:30:42 +0300

    test

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
 	"os"
 )
+
+var mp = map[string]tgbotapi.InlineKeyboardMarkup{
+	"start": tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData(
+				"Find repeatable signers on different accounts",
+				"Write 2 or more accounts and their transactions",
+			),
+		),
+	),
+}
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
@@ -25,13 +35,40 @@ func main() {
 	}
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		if update.Message != nil {
+			if update.Message.Text != "/start" {
+				continue
+			}
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("privet"))
-			msg.ReplyToMessageID = update.Message.MessageID
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			switch update.Message.Text {
+			case "/start":
+				msg.ReplyMarkup = mp["start"]
+			}
 
-			bot.Send(msg)
+			// Send the message.
+			if _, err = bot.Send(msg); err != nil {
+				panic(err)
+			}
+		} else if update.CallbackQuery != nil {
+			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+			if _, err := bot.AnswerCallbackQuery(callback); err != nil {
+				panic(err)
+			}
+
+			msg := getMessage(update.CallbackQuery)
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
 		}
 	}
+}
+
+func getMessage(callback *tgbotapi.CallbackQuery) tgbotapi.Chattable {
+	newMarkup, ok := mp[callback.Data]
+	if ok {
+		return tgbotapi.NewEditMessageReplyMarkup(callback.Message.Chat.ID, callback.Message.MessageID, newMarkup)
+	}
+
+	return tgbotapi.NewMessage(callback.Message.Chat.ID, callback.Data)
 }
